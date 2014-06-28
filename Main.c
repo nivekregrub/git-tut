2b04b413849766e6e30030882b34895a25a3af74
Kevin Burger <burgerk@asu.edu>
2014-06-27 19:14:36 -0700

    First commit.

---
+++ Main.c
@@ -0,0 +1,46 @@
+/***************************************************************************************************
+ * FILE: Main.c
+ **************************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "CtoF.h"
+#include "FtoC.h"
+#include "FtoK.h"
+
+#define streq(s1, s2)  (strcmp((s1), (s2)) == 0)
+#define strneq(s1, s2) (strcmp((s1), (s2)) != 0)
+
+static void Help(int pExitCode, char *pExitMsg);
+static double StrToDouble(char *pNumber);
+
+static void Help(int pExitCode, char *pExitMsg)
+{
+    printf("tempc(%d): %s\n", pExitCode, pExitMsg);
+    printf("Usage: tempc -f|-c number\n");
+    printf("Options:\n");
+    printf("    -f   Convert temperature from Fahr to Cels and Kelvin.\n");
+    printf("    -c   Convert temperature from Cels to Fahr.\n");
+    exit(pExitCode);
+}
+
+int main(int pArgc, char *pArgv[])
+{
+     if (pArgc != 3) Help(-1, "Incomplete command line.");
+    if (strneq(pArgv[1], "-f") && strneq(pArgv[1], "-c")) {
+        Help(-2, "Invalid option on command line.");
+    }
+    double temp = StrToDouble(pArgv[2]);
+    if (streq(pArgv[1], "-f")) printf("%.3lf C, %.3lf K\n", FtoC(temp), FtoK(temp));
+    else printf("%.3lf\n", CtoF(temp));
+    return 0;
+}
+
+static double StrToDouble(char *pNumber)
+{
+    double number;
+    if (streq(pNumber, "0") || streq(pNumber, "0.") || streq(pNumber, "0.0")) return 0;
+    number = atof(pNumber);
+    if (number == 0) Help(-3, "Invalid numeric temperature.");
+    return number;
+}
---
+++ Makefile
@@ -0,0 +1,46 @@
+#!/bin/bash
+#***************************************************************************************************
+# Makefile
+#***************************************************************************************************
+
+# The name of the executable file (called a binary).
+BINARY = tempc
+
+# Options for the C compiler.
+CFLAGS = -ansi -c -g -O0 -Wall
+
+# a list of the .c files in the project.
+SOURCES = CtoF.c    \
+          FtoC.c    \
+          FtoK.c    \
+          Main.c
+
+# A list of the .o files in the project. Replaces all occurrences of .c in the SOURCES macro with
+# .o, i.e., OBJECTS will be assigned: CtoF.o, FtoC.o, and Main.o.
+OBJECTS = $(SOURCES:.c=.o)
+
+# A list of the .d dependency files in the project.
+DEPS = $(SOURCES:.c=.d)
+
+# The binary depends on the object files. Link the object files to produce the binary.
+$(BINARY): $(OBJECTS)
+	gcc $(OBJECTS) -o $(BINARY)
+
+# Even though there is an implicit rule which states that the .o files depend on the. c files, I
+# like to be explicit about it. Compile a .c file to produce a .o file.
+%.o: %.c
+	gcc $(CFLAGS) $< -o $@
+
+# The .d files depend on the .c files. Ccreate a .d file by running gcc –MM.
+%.d: %.c
+	rm -f $@; gcc -MM $< > $@
+
+# Include the contents of all of the .d files.
+include $(DEPS)
+
+# "clean" is a phony target. Delete all of the .o and .d files and the binary.
+.PHONY: clean
+clean:
+	rm -f $(OBJECTS)
+	rm -f $(DEPS)
+	rm -f $(BINARY)
